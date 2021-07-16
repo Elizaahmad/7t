c389458d79ac0952f529f5b778c80b22e1dca008
Elizaahmad <74451187+Elizaahmad@users.noreply.github.com>
2021-07-16 18:12:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,58 +1,46 @@
-video="";
-status="";
-object=[];
-function preload()
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+  video.size(800,400);
+
+  video.parent("game_console");
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+	game()
+}
+function modelLoaded(){
+	console.log("model Loaded");
+}
+
+function gotPoses(results)
 {
-    video=createVideo('Video.webm');
-video.hide();
+  if(results.length > 0)
+  {
+	  console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    
+  }
 }
 
-function setup(){
-canvas=createCanvas(490,400);
-canvas.position(490,270);
-}
-function draw(){
-    image(video,0,0,490,400);
-    if(status!="")
-    {
-        objectDetector.detect(video,gotresults);
-        for(i=0;i<object.length;i++)
-        {
-       document.getElementById("Status").innerHTML="Status:Objects Detected";
-       document.getElementById("number_of_object").innerHTML="Number of objects detected are "+object.length;
 
-       r=random(255);
-       g=random(255);
-       b=random(255);
-       fill(r,g,b);
-            percent=floor(object[i].confidence*100);
-        text(object[i].label+" "+percent+"%", object[i].x,object[i].y);
-        noFill();
-        stroke(r,g,b);
-        rect(object[i].x,object[i].y,object[i].width,object[i].height);
-        }
-    }
-      
-}
 
-function Start(){
-    objectDetector=ml5.objectDetector('cococssd',modelloaded);
-    document.getElementById('status').innerHTML='Status:Detecting Objects';
-}
-function modelloaded(){
-    console.log('modelloaded');
-    status=true;
-    video.loop();
-    video.speed(0.5);
-    video.volume(0);
-}
-function gotresults(error,results){
-if (error){
-    console.log(error);
-}
-else {
-    console.log(results);
-    object=results;
 
-}
-}
+
+
+
+
+
+
